17eb775ada1f843827ac15f7bf4aa699e9a36c11
asayda01 <ahmetcemsaydam@gmail.com>
2023-09-27 09:04:59 +0100

    added the Frog Jump exercise
    added the Frog Jump exercise and its solution in Java .
    
    added the Frog Jump exercise and its solution in Java .

---
+++ Lessons/Lesson - 3 - Time Complexity/FrogJmp.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public int solution(int X, int Y, int D) {
+
+    if ( ( Y - X ) % D == 0 ) {
+        return ( Y - X ) / D ;
+    } ;
+
+    return ( Y - X ) / D + 1 ;
+
+    } ;
+
+} ;
